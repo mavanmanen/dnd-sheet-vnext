bd1403026c71061d5bb74edd8db8d71446ee8544
Mitchell van Manen <ma.van.manen@live.nl>
2023-04-10 14:45:54 +0200

    update

--- api/Swagger.cs
+++ api/Swagger.cs
@@ -9,12 +9,12 @@ namespace DndSheetApi;
 
 public class Swagger
 {
-    [FunctionName("SwaggerJson"), SwaggerIgnore]
+    [FunctionName("SwaggerJson"), SwaggerIgnore, Disable("SwaggerDisabled")]
     public static Task<HttpResponseMessage> SwaggerJsonAsync(
         [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "swagger.json")] HttpRequestMessage req, [SwashBuckleClient] ISwashBuckleClient swashBuckleClient) =>
         Task.FromResult(swashBuckleClient.CreateSwaggerJsonDocumentResponse(req));
     
-    [FunctionName("SwaggerUI"), SwaggerIgnore]
+    [FunctionName("SwaggerUI"), SwaggerIgnore, Disable("SwaggerDisabled")]
     public Task<HttpResponseMessage> SwaggerUIAsync(
         [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "swaggerUI")] HttpRequestMessage req, [SwashBuckleClient] ISwashBuckleClient swashBuckleClient) =>
         Task.FromResult(swashBuckleClient.CreateSwaggerUIResponse(req, "swagger.json"));
